3c92f8e3a050577f8f3cccb9c254427be6e2d3bf
Davi Barbosa <137322506+dontknowg@users.noreply.github.com>
2026-07-06 01:13:07 -0300

    Add TEMAS_POR_LIVRO dictionary with topics

---
+++ temas.py
@@ -0,0 +1,40 @@
+TEMAS_POR_LIVRO = {
+    "Livro 1": [
+        "Inclusão da população LGBTQIA+ em sociedade",
+        "Desvalorização do letramento digital no Brasil",
+        "Valorização da sustentabilidade no Brasil",
+        "Combate às ISTs entre a população brasileira"
+    ],
+    "Livro 2": [
+        "Imunização infantil na sociedade brasileira",
+        "Fortalecimento da doação de sangue no Brasil",
+        "Automedicação no Brasil",
+        "A importância da garantia de saúde bucal no Brasil"
+    ],
+    "Livro 3": [
+        "Desafios no combate à nomofobia em sociedade",
+        "Formas de enfrentamento aos crimes digitais",
+        "Consequências da exposição digital de jovens",
+        "Caminhos para a regulamentação de IAs no Brasil",
+        "Perspectivas acerca do trabalho na Era Digital"
+    ],
+    "Livro 4": [
+        "Democratização do acesso à literatura no Brasil",
+        "Preservação do patrimônio histórico-cultural",
+        "Perspectivas de valorização da cultura nacional",
+        "Desafios para a valorização da arte de periferia"
+    ],
+    "Livro 5": [
+        "Combate ao descarte inadequado de resíduos",
+        "Fortalecimento da agricultura sustentável",
+        "Combate à crise climática no Brasil",
+        "Desafios no combate ao racismo ambiental"
+    ],
+    "Livro 6": [
+        "Formas de enfrentamento à violência escolar",
+        "Fortalecimento da busca por desaparecidos no Brasil",
+        "Perspectivas de superação da violência policial",
+        "Caminhos para o combate ao feminicídio no Brasil",
+        "Desafios no enfrentamento à transfobia no Brasil"
+    ]
+}
